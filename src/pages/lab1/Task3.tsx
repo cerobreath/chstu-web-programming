4c35ee8fc74b046a789ca9ffc629e26b03510754
Dylan <cerobreath@gmail.com>
2025-02-15 20:36:59 +0200

    A billet for the third assignment.

--- src/pages/lab1/Task3.tsx
+++ src/pages/lab1/Task3.tsx
@@ -1,3 +1,11 @@
+import GreenDay from './assets/AmericanIdiot.jpg';
+
 export default function Task3() {
-    return <p>Это задание 3</p>;
+    return (
+        <div>
+            <title>Лабораторна 1. Завдання 3.</title>
+            <h1 className={"header-site"}>Завдання 3. Додавання зображення</h1>
+            <img src={GreenDay} alt={"American Idiot"}/>
+        </div>
+    );
 }
